a8c911aac327257798e4aa97735466875151e323
behe-lit <simazeri@naver.com>
2020-01-30 23:10:34 +0900

    버블정렬 완성

--- src/DataStructure/BubbleSort.java
+++ src/DataStructure/BubbleSort.java
@@ -3,7 +3,25 @@ package DataStructure;
 public class BubbleSort {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int array[] = {5,4,3,2,1};
+
+		int temp;
+		int i;
+		int j;
+
+		for(i=0; i<array.length-1; i++) {
+			for(j=i+1; j<array.length; j++) {
+				if(array[i]>array[j]) {
+					temp = array[i];
+					array[i] = array[j];
+					array[j] = temp;
+				}
+			}
+		}
+
+		for(i=0; i<array.length; i++) {
+			System.out.print("array["+i+"]"+"\t");
+		}
 
 	}
 
